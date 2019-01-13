0c77cb4d9316da31fa23ae8ce37579ca57692378
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-01-13 16:48:24 -0200

    user role inside token

--- api/src/schemas/MySchema/User/UseCases/LoginUser.js
+++ api/src/schemas/MySchema/User/UseCases/LoginUser.js
@@ -4,12 +4,9 @@ import jwt from 'jsonwebtoken'
 
 export default async (obj, params, ctx, resolveInfo) => {
     params.loginUserInput.password = crypto.createHmac('sha256', process.env.JWT_SECRET).update(params.loginUserInput.password).digest('hex')
-    // let Neo4jResponse = neo4jgraphql(obj, params, ctx, resolveInfo, true)
-    // console.log(Neo4jResponse)
-    // return Neo4jResponse
 
     const session = ctx.driver.session();
-    const query = "MATCH (u:User {email: $loginUserInput.email, password: $loginUserInput.password}) RETURN u"
+    const query = "MATCH (u:User {email: $loginUserInput.email, password: $loginUserInput.password})-[p:PLAYS]->(r:Role) RETURN u,p,r"
     return session.run(query, params)
         .then( result => {
             if(result.records.length > 0) {
@@ -18,12 +15,11 @@ export default async (obj, params, ctx, resolveInfo) => {
                     email: result.records[0].get('u').properties.email,
                     first_name: result.records[0].get('u').properties.first_name,
                     last_name: result.records[0].get('u').properties.last_name,
+                    role: [result.records[0].get('r').properties.name]
                 }
-                console.log(user)
                 const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: process.env.TOKEN_EXPIRE_IN })
                 return {token: token}
-            }
-            else {
+            } else {
                 throw new ApolloError('user_or_password_incorrect', 405, ['User or password is incorrect']);
             }
         })
