8a04dc3f9d8171acee4a49b427c4b2eb3e06427f
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-01-14 17:28:08 -0200

    show movie in ui

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,11 +3,11 @@ import { render } from 'react-dom';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from 'react-apollo';
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
-import Users from './users'
+import Movies from './movies'
 import { API } from './config'
 
 // const token = localStorage.getItem('token'); 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzNjk5YzkyLWM3ZGUtNDAzNy05YzZkLWExODA2ZTJiYzI2YSIsImVtYWlsIjoiYnJlbm8ubWF6aWVpcm9AZ21haWwuY29tIiwiZmlyc3RfbmFtZSI6IkJyZW5vIG91dHJvIiwibGFzdF9uYW1lIjoiTWF6aWVpcm8iLCJyb2xlIjpbIkFETUlOIl0sImlhdCI6MTU0NzQxMDM5OSwiZXhwIjoxNTQ3NTgzMTk5fQ.hxQugMHf-X_3ijdfHN9LeTKxfSxgLpppiwFbkqspBqI"
+const token = false
 const client = new ApolloClient({
   uri: API,
   headers: {
@@ -19,7 +19,7 @@ const App = () => (
   <ApolloProvider client={client}>
     <ApolloHooksProvider client={client}>
       <Suspense fallback={<div>Loading...</div>}>
-        <Users />
+        <Movies />
       </Suspense>
    </ApolloHooksProvider>
   </ApolloProvider>
---
+++ ui/src/movies.js
@@ -0,0 +1,59 @@
+import gql from 'graphql-tag';
+import React from 'react';
+import { useQuery } from 'react-apollo-hooks';
+
+let movieTitle = 'R'
+
+const GET_MOVIES = gql`
+{
+  MoviesByTitle(substring: "${movieTitle}")
+  {
+    id
+    title
+    year
+    plot
+    poster
+    imdbRating
+    created {
+      formatted
+    }
+    updated {
+      formatted
+    }
+  }
+}
+`;
+
+const Users = () => {
+  const { data, error } = useQuery(GET_MOVIES);
+  if (error) return `Error! ${error.message}`;
+  
+  if(data.MoviesByTitle.length > 0) {
+    return (
+      <div>
+        <ul>
+          {
+            data.MoviesByTitle.map(movie => (
+              <div key={movie.id}>
+                <li>Id: {movie.id} </li>
+                <li>Title: {movie.title} </li>
+                <li>Year: {movie.year} </li>
+                <li>Plot: {movie.plot} </li>
+                <li>Poster: <img src={movie.poster} alt=""></img> </li>
+                <li>ImdbRating: {movie.imdbRating} </li>
+              </div>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <ul>
+        there is no user yet!
+      </ul>
+  );
+  }
+};
+
+export default Users;
