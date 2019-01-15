c5ecfb7cfbdfd8c579785280ad78a353d7caa413
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-01-14 22:55:14 -0200

    create seed admin user

--- api/src/seed/seed-db.js
+++ api/src/seed/seed-db.js
@@ -1,18 +1,26 @@
-import ApolloClient from "apollo-client";
-import gql from "graphql-tag";
-import seedmutations from "./seed-mutations";
-import fetch from "node-fetch";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import {driver} from '../infra/databases/database'
+import crypto from 'crypto'
 
-const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.GRAPHQL_URI, fetch }),
-  cache: new InMemoryCache()
-});
+const adminEmail = process.env.ADMIN_EMAIL
+const adminPassw = crypto.createHmac('sha256', process.env.JWT_SECRET).update(process.env.ADMIN_PASSWORD).digest('hex')
 
-client
-  .mutate({
-    mutation: gql(seedmutations)
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+const createAdmin = driver.session().run(
+  `MERGE (u:User {email: "${adminEmail}"})
+  ON CREATE SET
+    u.id = apoc.create.uuid(),
+    u.first_name = "Admin",
+    u.last_name = "Mycompany",
+    u.email = "${adminEmail}",
+    u.password = "${adminPassw}",
+    u.created = datetime(),
+    u.softDeleted = false
+  ON MATCH SET
+    u.first_name = "Admin",
+    u.last_name = "Mycompany",
+    u.password = "${adminPassw}",
+    u.updated = datetime()
+  MERGE (r:Role {name: 'ADMIN'})
+  MERGE (u)-[p:PLAYS]->(r)
+  RETURN u`
+).then( result => { console.log('\x1b[32m',`Admin user was Successfully created! ${adminEmail} : ${process.env.ADMIN_PASSWORD}`); process.exit();})
+.catch((e) => { console.log('\x1b[31m','Something went wrong!',e); process.exit();})
