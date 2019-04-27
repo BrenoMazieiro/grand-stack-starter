9a6fdacd76b779d95d2ad3b327fe50633d654e5e
Breno Mazieiro <breno.mazieiro@plathanus.com.br>
2019-04-26 21:33:33 -0300

    compressing

--- api/src/index.js
+++ api/src/index.js
@@ -1,29 +1,4 @@
-/*
- * Primary file for API
- *
- */
-
-// Dependencies
 import server from './interfaces/http/server'
 import workers from'./interfaces/background/workers'
 
-// Declare the app
-var app = {}
-
-// Init function
-app.init = function(){
-
-  // Start the server
-  server.init()
-
-  // Start the workers
-  workers.init()
-
-}
-
-// Self executing
-app.init()
-
-
-// Export the app
-module.exports = app
+(() => { server.init(); workers.init()})()
