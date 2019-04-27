51698be6747870520d295eaa818203cb9ed091cd
Breno Mazieiro <breno.mazieiro@plathanus.com.br>
2019-04-26 21:34:59 -0300

    Using promise

--- api/src/schemas/MySchema/Movie/UseCases/CreateMovie.js
+++ api/src/schemas/MySchema/Movie/UseCases/CreateMovie.js
@@ -1,17 +1,17 @@
 import { neo4jgraphql } from "neo4j-graphql-js";
 import { ApolloError } from "apollo-server"
-import crypto from 'crypto'
 
 export default async (obj, params, ctx, resolveInfo) => {
   if (ctx.user.role.includes('ADMIN')) {
-    const findMovie = await ctx.driver.session().run(
+    return ctx.driver.session().run(
       `MATCH (m:Movie {title: "${params.searchMovieInput.title}", year: "${params.searchMovieInput.year}"}) return m`
-    )
-    if (findMovie.records.length > 0) {
-      throw new ApolloError('movie_already_exists', 200, 'This movie already exists');
-    } else {
-      return neo4jgraphql(obj, params, ctx, resolveInfo, true)
-    }
+    ).then(async data => {
+      if (data.records.length) {
+        throw new ApolloError('movie_already_exists', 200, 'This movie already exists');
+      } else {
+        return neo4jgraphql(obj, params, ctx, resolveInfo)
+      }
+    }).catch(e =>{console.log('===>',e)})
   } else {
     throw new ApolloError('not_authorized', 405, ['You are not allowed to do that']);
   }
