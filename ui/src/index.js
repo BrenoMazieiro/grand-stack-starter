0004c28d7e62f7524706da09ce96241604e9b215
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-03-26 22:18:43 +0000

    Updating dependencies

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,18 +1,18 @@
-import React, { Suspense } from 'react';
-import { render } from 'react-dom';
+import React, { Suspense } from "react";
+import { render } from "react-dom";
 import ApolloClient from "apollo-boost";
-import { ApolloProvider } from 'react-apollo';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
-import Movies from './movies'
-import { API } from './config'
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import Movies from "./movies";
+import { API } from "./config";
 
-// const token = localStorage.getItem('token'); 
-const token = false
+// const token = localStorage.getItem('token');
+const token = false;
 const client = new ApolloClient({
   uri: API,
   headers: {
-          authorization: token ? token : "",
-        }
+    authorization: token ? token : ""
+  }
 });
 
 const App = () => (
@@ -21,8 +21,8 @@ const App = () => (
       <Suspense fallback={<div>Loading...</div>}>
         <Movies />
       </Suspense>
-   </ApolloHooksProvider>
+    </ApolloHooksProvider>
   </ApolloProvider>
 );
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
--- ui/src/movies.js
+++ ui/src/movies.js
@@ -1,10 +1,10 @@
-import gql from 'graphql-tag';
-import React from 'react';
-import { useQuery } from 'react-apollo-hooks';
+import gql from "graphql-tag";
+import React from "react";
+import { useQuery } from "react-apollo-hooks";
 
-let movieTitle = 'a'
+let movieTitle = "a";
 const styleCard = {
-  width: '18rem'
+  width: "18rem"
 };
 const GET_MOVIES = gql`
 {
@@ -26,35 +26,35 @@ const GET_MOVIES = gql`
 }
 `;
 
-const Users = () => {
+const Movies = () => {
   const { data, loading, error } = useQuery(GET_MOVIES);
-  if (loading) return 'Loading';
+  if (loading) return "Loading";
   if (error) return `Error! ${error.message}`;
 
   if (data.MoviesByTitle.length > 0) {
     return (
       <div class="card-columns">
-          {
-            data.MoviesByTitle.map(movie => (
-              <div class="card" style={styleCard} key={movie.id}>
-                <img src={movie.poster} class="card-img-top" alt="..."></img>
-                  <div class="card-body">
-                    <h5 class="card-title">{movie.title} ({movie.year}) - {movie.imdbRating} </h5>
-                    <p class="card-text">{movie.plot}</p>
-                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                  </div>
-              </div>
-                ))
-              }
+        {data.MoviesByTitle.map(movie => (
+          <div class="card" style={styleCard} key={movie.id}>
+            <img src={movie.poster} class="card-img-top" alt="..."></img>
+            <div class="card-body">
+              <h5 class="card-title">
+                {movie.title} ({movie.year}) - {movie.imdbRating}{" "}
+              </h5>
+              <p class="card-text">{movie.plot}</p>
+              <a href="/" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
-        );
+    );
   } else {
     return (
-      <ul>
-        There is no Movie Yet, maybe if you create some it will be here!
-      </ul>
-          );
-          }
-        };
-        
-export default Users;
+      <ul>There is no Movie Yet, maybe if you create some it will be here!</ul>
+    );
+  }
+};
+
+export default Movies;
